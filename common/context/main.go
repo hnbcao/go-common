5082c60fd9ad3a1f19815a3fea3c49f786645fac
hnbcao <hnbcao@gmail.com>
2021-02-28 21:22:18 +0800

    init

--- common/context/main.go
+++ common/context/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 func main() {
-	$END$
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	go handle(ctx, 500*time.Millisecond)
+	select {
+	case <-ctx.Done():
+		fmt.Println("main", ctx.Err())
+	}
+}
+
+func handle(ctx context.Context, duration time.Duration) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("handle", ctx.Err())
+		//case <-time.After(duration):
+		//	fmt.Println("process request with", duration)
+		}
+	}
+
 }
